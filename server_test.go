3b9ee2a72ef6133558bcc6e7a5836ab968e668da
egasetiawan <ega.rizky.setiawan@mail.ugm.ac.id>
2023-12-18 15:36:47 +0700

    add: request

--- server_test.go
+++ server_test.go
@@ -40,3 +40,20 @@ func TestServeMux(t *testing.T) {
 	}
 
 }
+
+func TestRequest(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, r.Method)
+		fmt.Fprintln(w, r.RequestURI)
+	})
+	server := http.Server{
+		Addr:    "localhost:5000",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
